4b1d0a9bd25cf0217fb667016db330e14edf52de
Dikshasharmaa <diksha66sharma78@gmail.com>
2024-04-05 22:15:53 -0400

    Added properties for Sneakers class

--- src/main/java/Models/Sneakers.java
+++ src/main/java/Models/Sneakers.java
@@ -1,4 +1,11 @@
 package Models;
 
 public class Sneakers {
+    private int id;
+    private String name;
+    private String brand;
+    private String sport;
+    private float size;
+    private int quantity;
+    private float price;
 }
